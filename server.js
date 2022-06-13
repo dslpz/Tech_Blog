39954d363142cb1a54dca036572df822426ea1f1
Damaris Palacios <dslpz728@gmail.com>
2022-06-12 23:35:04 -0500

    saves progress

--- server.js
+++ server.js
@@ -4,15 +4,13 @@ const session = require('express-session');
 
 const app = express();
 
-// config folder
     store: new SequelizeStore({
         db: sequelize
     })
-};
 
 app.use(session(sess));
 
-// utils folder
+
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
@@ -21,12 +19,11 @@ const hbs = exphbs.create({ helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// public folder
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// controllers folder
 app.use(require('./controllers/'));
 
 app.listen(process.env.PORT || 3001, () => {
