c5c7c5af2e870513360b7071932703b82d6eb1dc
Damaris Palacios <dslpz728@gmail.com>
2022-06-12 19:47:23 -0500

    adds to folders

---
+++ controllers/api/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const apiRoutes = require('./api/');
+const homeRoutes = require('./home_routes.js');
+const dashboardRoutes = require('./dashboard_routes.js');
+
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
